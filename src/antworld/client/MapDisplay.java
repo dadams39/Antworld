a9cdc84ea7870bd55cea2894f2876a10679186af
Danny Adams <dadams39@unm.edu>
2014-09-03 18:46:24 -0600

    Changed Map Client as to suppress warnings.

--- src/antworld/client/MapDisplay.java
+++ src/antworld/client/MapDisplay.java
@@ -1,28 +1,20 @@
 package antworld.client;
 
-
-
-public class MapDisplay {
-  
-  /********************************************/
-  /* A_Star_Rendering.java                    */
-  /*  written by Danny Adams                  */
-  /*  Updated August 21, 2014                 */
-  /********************************************/
- 
+public class MapDisplay 
+{
+  /********************************/
+  /* MapDisplay.java              */
+  /*  written by Danny Adams      */
+  /*  Updated August 21, 2014     */
+  /********************************/
   public MapDisplay ()
   {
-
-    
-      String file = "/nfs/student/d/dadams39/cs351/AntWorld/AntWorld.png";
-      new Picture(file);
-
-      
-    }
+    String file = "/nfs/student/d/dadams39/cs351/AntWorld/AntWorld.png";
+    new Picture(file);
+  }
   public static void main(String args[])
   {
-    
-    MapDisplay trial = new MapDisplay();
+    new MapDisplay();
   }
 
 }
