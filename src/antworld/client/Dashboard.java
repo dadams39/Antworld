1f451982684d5861ecfaa498d9067928f5dec2b8
Danny Adams <dadams39@unm.edu>
2014-09-03 18:45:11 -0600

    Added extra column for functionability to show whether or not food has been located.

--- src/antworld/client/Dashboard.java
+++ src/antworld/client/Dashboard.java
@@ -19,16 +19,22 @@ import antworld.data.CommData;
 @SuppressWarnings("serial")
 public class Dashboard  extends JFrame
 {
-  // Instance attributes used in this example
+  // Instance attributes 
   private JPanel topPanel;
   private JTable table;
   private JScrollPane scrollPane;
 
   public Dashboard()
+  /************************************************/
+  /* Dashboard()                                  */
+  /* @ author: Danny Adams                        */
+  /* @ modified by CS351 Team.                    */
+  /*   Basic Constructor.                         */
+  /*   Used for initiation of JTable in program   */
+  /************************************************/
   {
-    // Set the frame characteristics
-
-    this.setTitle( "Simple Table Application" );
+    //Frame settings
+    this.setTitle( "JTable Set-up" );
     this.setSize( 300, 200 );
     this.setBackground( Color.gray );
 
@@ -38,27 +44,34 @@ public class Dashboard  extends JFrame
     this.getContentPane().add( topPanel );
 
     // Create columns names
-    String columnNames[] = { "Column 1", "Column 2", "Column 3" };
+    String columnNames[] = { "Name", "Age", "State" };
 
-    // Create some data
+    // Corresponding Data Entries
     String dataValues[][] =
       {
-        { "12", "234", "67" },
-        { "-123", "43", "853" },
-        { "93", "89.2", "109" },
-        { "279", "9033", "3092" }
+        { "Danny", "33", "New Mexico" },
+        { "Bob", "43", "Delaware" },
+        { "Trinity", "19", "Utah" },
+        { "Brandon", "20", "Florida" }
       };
 
-    // Create a new table instance
+    // JTable instance
     table = new JTable( dataValues, columnNames );
 
-    // Add the table to a scrolling pane
+    // Adding a scrolling pane
     scrollPane = new JScrollPane( table );
     topPanel.add( scrollPane, BorderLayout.CENTER );
 
   }
 
   public Dashboard(CommData commData)
+  /************************************************/
+  /* Dashboard(CommData commData)                 */
+  /* @ author: Danny Adams                        */
+  /* @ modified by CS351 Team.                    */
+  /*   Constructor utilized in Ant_Project        */
+  /*   Used for tracking of ants in  JTable       */
+  /************************************************/
   {
     // Set the frame characteristics
 
@@ -72,19 +85,20 @@ public class Dashboard  extends JFrame
     this.getContentPane().add( topPanel );
 
     // Create columns names
-    String columnNames[] = { "Ant_Name", "X Coord.", "Y Coord." };
+    String columnNames[] = { "Ant_Name", "X Coord.", "Y Coord.", "Units" };
 
     int column = commData.myAntList.size();
-    String ant_Values[][] = new String [column][3];
+    String ant_Values[][] = new String [column][4];
     int index = 0;
     for(AntData ant : commData.myAntList)
     {
       ant_Values[index][0] = "Ant_" + Integer.toString(index);
       ant_Values[index][1] = Integer.toString(ant.gridX);
       ant_Values[index][2] = Integer.toString(ant.gridY);
+      ant_Values[index][3] = Integer.toString(ant.carryUnits);
       index ++;
     }
-   
+
     // Create a new table instance
     table = new JTable( ant_Values, columnNames );
 
@@ -93,37 +107,44 @@ public class Dashboard  extends JFrame
     topPanel.add( scrollPane, BorderLayout.CENTER );
 
   }
-  
+
   public void updateTable(CommData commData)
-  
+  /************************************************/
+  /* updateTable(CommData commData)               */
+  /* @ param: CommData object[Antworld project]   */
+  /* @ Returns: N/A                               */
+  /*   Updates location of ants within tables.    */
+  /*   Used for tracking of ants in  JTable       */
+  /************************************************/
   {
     this.topPanel = new JPanel();
     this.topPanel.setLayout( new BorderLayout());
     this.getContentPane().add( topPanel );
-  
-    String columnNames[] = { "Ant_Name", "X Coord.", "Y Coord." };
+
+    String columnNames[] = { "Ant_Name", "X Coord.", "Y Coord.", "Units" };
 
     int column = commData.myAntList.size();
-    String ant_Values[][] = new String [column][3];
+    String ant_Values[][] = new String [column][4];
     int index = 0;
     for(AntData ant : commData.myAntList)
     {
       ant_Values[index][0] = "Ant_" + Integer.toString(index);
       ant_Values[index][1] = Integer.toString(ant.gridX);
       ant_Values[index][2] = Integer.toString(ant.gridY);
+      ant_Values[index][3] = Integer.toString(ant.carryUnits);
+      
       index ++;
     }
-   
+
     // Create a new table instance
     table = new JTable( ant_Values, columnNames );
-
     // Add the table to a scrolling pane
     scrollPane = new JScrollPane( table );
     scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); 
     topPanel.add( scrollPane, BorderLayout.CENTER );
-    
+
   }
-//Simple Unit Test
+  //Simple Unit Test
   public static void main( String args[] )
   {
     // Create an instance of the test application
